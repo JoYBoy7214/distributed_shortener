3e8208be406c07d949c1df1d39dfacedbaa308f0
Gowtham Balaji <gowthambalaji1472@gmail.com>
2026-01-07 12:12:08 +0530

    swtiched to postgresql

--- internal/shortener/service.go
+++ internal/shortener/service.go
@@ -3,56 +3,70 @@ package shortener
 import (
 	"context"
 	"math/rand"
-	"sync"
+	"strings"
 	"time"
 
 	pb "github.com/JoYBoy7214/distributed_shortener/api/proto/v1"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 type Service struct {
 	pb.UnimplementedShortenerServer
-	m   map[string]string
-	mtx sync.RWMutex
+	pool *pgxpool.Pool
 }
 
-func NewService() *Service {
+func NewService(pool *pgxpool.Pool) *Service {
 	return &Service{
-		m: make(map[string]string),
+		pool: pool,
 	}
 }
+func (s *Service) CreateUrlTable() error {
+	_, err := s.pool.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS urls(id SERIAL UNIQUE,short_code VARCHAR(10) PRIMARY KEY UNIQUE NOT NULL,original_url TEXT NOT NULL)")
+	if err != nil {
+		return status.Errorf(codes.Internal, "failed to create the url table")
+	}
+	return nil
 
+}
 func (s *Service) CreateShortUrl(ctx context.Context, req *pb.CreateShortUrlRequest) (*pb.CreateShortUrlResponse, error) {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var shortID string
+	for i := 0; i < 5; i++ {
+		b := make([]byte, 10)
+		for j := range b {
+			b[j] = charset[rng.Intn(len(charset))]
+		}
+		shortID = string(b)
+		_, err := s.pool.Exec(ctx, "INSERT INTO urls (short_code, original_url) VALUES ($1, $2)", shortID, req.OriginalUrl)
+		if err != nil {
+			if strings.Contains(err.Error(), "SQLSTATE 23505") && i != 4 {
+				continue
+			} else {
+				return nil, status.Errorf(codes.Internal, "failed to insert the Url %s", req.OriginalUrl)
+			}
+		} else {
+			break
+		}
 
-	b := make([]byte, 10)
-	for i := range b {
-		b[i] = charset[rng.Intn(len(charset))]
 	}
-	shortID := string(b)
-
-	s.mtx.Lock()
-	s.m[shortID] = req.OriginalUrl
-	s.mtx.Unlock()
-
 	return &pb.CreateShortUrlResponse{
 		ShortUrl: shortID,
 	}, nil
 }
 
 func (s *Service) GetOriginalUrl(ctx context.Context, req *pb.GetOriginalUrlRequest) (*pb.GetOriginalUrlResponse, error) {
-	s.mtx.RLock()
-	originalURL, exists := s.m[req.ShortUrl]
-	s.mtx.RUnlock()
+	Row := s.pool.QueryRow(ctx, "SELECT original_url FROM urls WHERE short_code = $1", req.ShortUrl)
+	var originalUrl string
 
-	if !exists {
-		// Return a proper gRPC error code
-		return nil, status.Errorf(codes.NotFound, "short url %s not found", req.ShortUrl)
+	err := Row.Scan(&originalUrl)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "data not found for the url : %s", req.ShortUrl)
 	}
 
 	return &pb.GetOriginalUrlResponse{
-		OriginalUrl: originalURL,
+		OriginalUrl: originalUrl,
 	}, nil
 }
