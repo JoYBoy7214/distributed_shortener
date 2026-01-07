fccddfaadf09836f0629f7a9c827d7bcd6e3e2a6
Gowtham Balaji <gowthambalaji1472@gmail.com>
2026-01-07 12:12:08 +0530

    swtiched to postgresql

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
 	pb "github.com/JoYBoy7214/distributed_shortener/api/proto/v1"
 	service "github.com/JoYBoy7214/distributed_shortener/internal/shortener"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"google.golang.org/grpc"
 )
+
+const dbUrl = "postgres://ZORO:secretfornow@localhost:5432/shortener"
 
 func main() {
 	ln, err := net.Listen("tcp", ":50051")
@@ -15,7 +19,9 @@ func main() {
 		log.Fatal("failed to listen on 50051", err)
 	}
 	s := grpc.NewServer()
-	server := service.NewService()
+	pool, err := pgxpool.New(context.Background(), dbUrl)
+	server := service.NewService(pool)
+	server.CreateUrlTable()
 	pb.RegisterShortenerServer(s, server)
 	log.Println("server started on 50051")
 	if err := s.Serve(ln); err != nil {
